2c815ed9dbec738a6ef5c565c93560fb0d2aeac8
Asses <joapoh@gmail.com>
2016-08-23 14:37:40 +0800

    Added Inventory.cpp and Inventory.h for Inventory.

--- SP1Framework/ActFeed.cpp
+++ SP1Framework/ActFeed.cpp
@@ -337,7 +337,6 @@ void actfeed()
 			g_Console.writeToBuffer(c, "it bloody works!", 0x02);
 			break;
 		}
-<<<<<<< HEAD
 
 		c.Y = tempcoords;
 	}
@@ -432,13 +431,8 @@ void actfeed()
 	g_Console.writeToBuffer(c, "Name", 0x04);
 	c.Y += 1;
 	} */
-	
-=======
 
-		c.Y = tempcoords;
-	}
-
-	if (g_dElapsedTime >= g_dElapsedTimeTemp && levelfinish == 1)
+	if (g_dElapsedTime >= g_dElapsedTimeTemp && levelfinish == 1);
 	{
 		InPortal = 0;
 		Areanum = 1;
@@ -447,5 +441,4 @@ void actfeed()
 		g_sChar.m_cLocation.X = g_Console.getConsoleSize().X - 88;
 		g_sChar.m_cLocation.Y = g_Console.getConsoleSize().Y - 45;
 	}
->>>>>>> 542698bb6174e3dd72ac83e5e5ce9f3b285bf027
 }
---
+++ SP1Framework/Inventory.h
@@ -0,0 +1,12 @@
+#ifndef _INVENTORY_H
+#define _INVENTORY_H
+
+#include "Framework\console.h"
+#include "game.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+//void ();
+
+#endif // _INVENTORY_H
