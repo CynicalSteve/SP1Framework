f6e22f0771e22574854b37308abac220b71a6228
DESKTOP-027DMTP\Steven Ho <syong789@hotmail.com>
2016-08-30 09:39:57 +0800

    Pushing to pull

--- SP1Framework/BadEnd.cpp
+++ SP1Framework/BadEnd.cpp
@@ -1,6 +1,21 @@
 #include "BadEnd.h"
+extern Console g_Console;
 
 void BadEnd()
 {
+	COORD c;
 	
+	c.Y = 34;
+	if (g_dElapsedTime > g_dTime)
+	{
+		g_Console.writeToBuffer(c, "\"Who was that girl just now? Did you know her?\" Ava asked jealously.", 0x03);
+		c.Y += 2;
+	}
+	if (g_dElapsedTime > (g_dTime + 3.0))
+	{
+		g_Console.writeToBuffer(c, "\"Yeah, she was my friend from high school.\" Terence replied.", 0x03);
+		c.Y += 2;
+	}
+
+
 }
--- SP1Framework/BadEnd.h
+++ SP1Framework/BadEnd.h
@@ -1,8 +1,16 @@
 #ifndef _BADEND_H
 #define _BADEND_H
 #include "game.h"
-#include "MegaInteract.h"
+#include "Framework\console.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <sstream>
+#include <windows.h>
+#include "Windows.h"
+#include "MMSystem.h"
+#include "RenderJournal.h"
 
 using namespace std;
 void BadEnd();
