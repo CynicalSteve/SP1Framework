40d1619ddde2b368d41131748ad7fd00efe2b81d
WeiMingxiu <mingxiu26@gmail.com>
2016-08-31 16:16:27 +0800

    Changed color of text in instructions screen because Edward complained about it

--- SP1Framework/InstructScreen.cpp
+++ SP1Framework/InstructScreen.cpp
@@ -11,22 +11,22 @@ void InstructScreen()
 	g_Console.writeToBuffer(c, "INSTRUCTIONS", 0x03);
 	c.Y++;
 	c.X = 21;
-	g_Console.writeToBuffer(c, "Use WASD keys to move around, F key to interact and E to enter/use.", 0x09);
+	g_Console.writeToBuffer(c, "Use WASD keys to move around, F key to interact and E to enter/use.", 0x07);
 	c.Y++;
 	c.X = 37;
-	g_Console.writeToBuffer(c, "Press 'J' to access journal entries", 0x09);
+	g_Console.writeToBuffer(c, "Press 'J' to access journal entries", 0x07);
 	c.Y++;
 	c.X = 41;
-	g_Console.writeToBuffer(c, "Press 'P' to pause the game", 0x09);
+	g_Console.writeToBuffer(c, "Press 'P' to pause the game", 0x07);
 	c.Y++;
 	c.X = 43;
-	g_Console.writeToBuffer(c, "Press 'M' to mute music", 0x09);
+	g_Console.writeToBuffer(c, "Press 'M' to mute music", 0x07);
 	c.Y++;
 	c.X = 42;
-	g_Console.writeToBuffer(c, "Press 'N' to unmute music", 0x09);
+	g_Console.writeToBuffer(c, "Press 'N' to unmute music", 0x07);
 	c.Y += 2;
 	c.X = 37;
-	g_Console.writeToBuffer(c, "Press 'B' to go back to menu screen", 0x08);
+	g_Console.writeToBuffer(c, "Press 'B' to go back to menu screen", 0x09);
 	if (isKeyPressed(0x42))
 	{
 		clearScreen();
